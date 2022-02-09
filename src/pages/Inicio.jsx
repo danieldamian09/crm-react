8763941a513b1bdddcd704088bb1c0931517a8cb
danieldamian09 <humbertorivero9@gmail.com>
2022-02-09 20:53:25 -0300

    API call to save clients inthe local state

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import {useEffect, useState} from 'react';
 
 function Inicio() {
+
+  const [clientes, setClientes] = useState([])
+
+  const obtenerClientesAPI = async() => {
+    try {
+      const url = "http://localhost:4000/clientes"
+      const respuesta = await fetch(url)
+      const resultado = await respuesta.json()
+      setClientes(resultado)
+    } catch (error) {
+      
+    }
+  }
+
+  useEffect(() => {
+    obtenerClientesAPI()
+  }, [])
+  
+
   return (
     <div>
       <h1>Inicio</h1>

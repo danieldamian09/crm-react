311e063d95fde0aed08f706d04a5f89c9d5c2fe7
danieldamian09 <humbertorivero9@gmail.com>
2022-02-09 21:27:49 -0300

    displaying clients information in the table and add style

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
 
 const Cliente = ({cliente}) => {
-  return (
-    <div>{cliente.nombre}</div>
-  )
-}
+	const {nombre, empresa, email, telefono, notas, id} = cliente;
 
-export default Cliente
+	return (
+		<tr className=" border-b hover:bg-gray-50">
+			<td className="p-3">{nombre}</td>
+			<td className="p-3">
+				<p>
+					<span className=" text-gray-800 uppercase font-bold">Email:</span>
+					{email}
+				</p>
+				<p>
+					<span className=" text-gray-800 uppercase font-bold">Tel:</span>
+					{telefono}
+				</p>
+			</td>
+			<td className="p-3">{empresa}</td>
+			<td className="p-3">
+				<button
+					type="button"
+					className=" bg-yellow-500 hover:bg-yellow-600 block w-full text-white p-2 font-bold text-xs uppercase"
+				>
+					Editar
+				</button>
+				<button
+					type="button"
+					className=" bg-blue-600 hover:bg-blue-700 block w-full text-white p-2 font-bold text-xs uppercase mt-3"
+				>
+					Editar
+				</button>
+				<button
+					type="button"
+					className=" bg-red-600 hover:bg-red-700 block w-full text-white p-2 font-bold text-xs uppercase mt-3"
+				>
+					Eliminar
+				</button>
+			</td>
+		</tr>
+	);
+};
+
+export default Cliente;

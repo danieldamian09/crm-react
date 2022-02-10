ed13ecf715c15f574ff1b2a02029f9ded68ad0d4
danieldamian09 <humbertorivero9@gmail.com>
2022-02-09 21:07:31 -0300

    create table for clients

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,30 +1,47 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useState} from "react";
+import Cliente from "../components/Cliente";
 
 function Inicio() {
+	const [clientes, setClientes] = useState([]);
 
-  const [clientes, setClientes] = useState([])
+	const obtenerClientesAPI = async () => {
+		try {
+			const url = "http://localhost:4000/clientes";
+			const respuesta = await fetch(url);
+			const resultado = await respuesta.json();
+			setClientes(resultado);
+		} catch (error) {}
+	};
 
-  const obtenerClientesAPI = async() => {
-    try {
-      const url = "http://localhost:4000/clientes"
-      const respuesta = await fetch(url)
-      const resultado = await respuesta.json()
-      setClientes(resultado)
-    } catch (error) {
-      
-    }
-  }
+	useEffect(() => {
+		obtenerClientesAPI();
+	}, []);
 
-  useEffect(() => {
-    obtenerClientesAPI()
-  }, [])
-  
+	return (
+		<>
+			<h1 className="font-black text-4xl text-blue-900">Clientes</h1>
+			<p className=" mt-3">Administra tus clientes</p>
 
-  return (
-    <div>
-      <h1>Inicio</h1>
-    </div>
-  );
+			<table className="w-full mt-5 table-auto shadow bg-white">
+				<thead className=" bg-blue-800 text-white">
+					<tr>
+						<th className="p-2">Nombre</th>
+						<th className="p-2">Contacto</th>
+						<th className="p-2">Empresa</th>
+						<th className="p-2">Acciones</th>
+					</tr>
+				</thead>
+				<tbody>
+					{clientes.map((cliente) => (
+						<Cliente 
+              key={cliente.id}
+              cliente={cliente}
+            />
+					))}
+				</tbody>
+			</table>
+		</>
+	);
 }
 
 export default Inicio;

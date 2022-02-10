acb6211da7d9baba9643c766159a03799ef2949e
danieldamian09 <humbertorivero9@gmail.com>
2022-02-10 20:15:48 -0300

    conditionally display component information

--- src/pages/VerCliente.jsx
+++ src/pages/VerCliente.jsx
@@ -1,52 +1,61 @@
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
 
 const VerCliente = () => {
+	const [cliente, setCliente] = useState({});
 
-  const [cliente, setCliente] = useState({})
+	const {id} = useParams();
 
-  const {id} = useParams()
+	const obtenerClienteAPI = async () => {
+		try {
+			const url = `http://localhost:4000/clientes/${id}`;
+			const respuesta = await fetch(url);
+			const resultado = await respuesta.json();
+			setCliente(resultado);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  const obtenerClienteAPI = async() => {    
-    try {
-      const url = `http://localhost:4000/clientes/${id}`;
-      const respuesta = await fetch(url)
-      const resultado = await respuesta.json()
-      setCliente(resultado)     
-    } catch (error) {
-      console.log(error)
-    }
-  }
+	useEffect(() => {
+		obtenerClienteAPI();
+	}, []);
 
-  useEffect(() => {
-    obtenerClienteAPI()
-  }, [])
-  
+	return (
+		<div>
+			<h1 className="font-black text-4xl text-blue-900">
+				Ver cliente: {cliente.nombre}
+			</h1>
+			<p className=" mt-3">informacion del cliente</p>
 
-  return (
-    <div>
-      <p className=" text-2xl text-gray-600 mt-4">
-        <span className="  text-gray-800 uppercase font-bold">Cliente:</span>
-        {cliente.nombre}
-      </p>
-      <p className=" text-2xl text-gray-600 mt-4">
-        <span className="  text-gray-800 uppercase font-bold">Email:</span>
-        {cliente.email}
-      </p>
-      <p className=" text-2xl text-gray-600 mt-4">
-        <span className="  text-gray-800 uppercase font-bold">Teléfono:</span>
-        {cliente.telefono}
-      </p>
-      <p className=" text-2xl text-gray-600 mt-4">
-        <span className="  text-gray-800 uppercase font-bold">Empresa:</span>
-        {cliente.empresa}
-      </p>
-      <p className=" text-2xl text-gray-600 mt-4">
-        <span className="  text-gray-800 uppercase font-bold">Notas:</span>
-        {cliente.notas}
-      </p>
-    </div>
-  )
-}
+			<p className=" text-2xl text-gray-600 mt-10">
+				<span className="  text-gray-800 uppercase font-bold">Cliente:</span>
+				{cliente.nombre}
+			</p>
+			<p className=" text-2xl text-gray-600 mt-4">
+				<span className="  text-gray-800 uppercase font-bold">Email:</span>
+				{cliente.email}
+			</p>
+			{/* Mostar notas en caso de que existan */}
+			{cliente.telefono && (
+				<p className=" text-2xl text-gray-600 mt-4">
+					<span className="  text-gray-800 uppercase font-bold">Teléfono:</span>
+					{cliente.telefono}
+				</p>
+			)}
+			<p className=" text-2xl text-gray-600 mt-4">
+				<span className="  text-gray-800 uppercase font-bold">Empresa:</span>
+				{cliente.empresa}
+			</p>
+			{/* Mostar notas en caso de que existan */}
+			{cliente.otas && (
+				<p className=" text-2xl text-gray-600 mt-4">
+					<span className="  text-gray-800 uppercase font-bold">Notas:</span>
+					{cliente.notas}
+				</p>
+			)}
+		</div>
+	);
+};
 
-export default VerCliente
+export default VerCliente;

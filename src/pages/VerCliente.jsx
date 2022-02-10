a015fe6e09fe651622fb3458ae6f9f86531aea8b
danieldamian09 <humbertorivero9@gmail.com>
2022-02-10 19:40:16 -0300

    requesting the API

--- src/pages/VerCliente.jsx
+++ src/pages/VerCliente.jsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 const VerCliente = () => {
 
+  const [cliente, setCliente] = useState({})
+
   const {id} = useParams()
 
-  console.log(id)
+  const obtenerClienteAPI = async() => {    
+    try {
+      const url = `http://localhost:4000/clientes/${id}`;
+      const respuesta = await fetch(url)
+      const resultado = await respuesta.json()
+      setCliente(resultado)     
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    obtenerClienteAPI()
+  }, [])
+  
 
   return (
     <div>
